83e7eee281fe1b3e091a53bc2a04d5783a8406d3
SC-201606291726\Administrator <1029654986@qq.com>
2017-03-07 10:38:52 +0800

    vision01

---
+++ main.js
@@ -0,0 +1,5 @@
+/**
+ * Created by Administrator on 2017/3/6 0006.
+ */
+
+console.log(require('./support'));
---
+++ support.js
@@ -0,0 +1,6 @@
+/**
+ *
+ * Created by Administrator on 2017/3/6 0006.
+ */
+
+module.exports='im support';
