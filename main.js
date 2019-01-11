764261309661736ad40bf69433571ffaa271c07c
zhouqing <1029654986@qq.com>
2019-01-11 16:23:44 +0800

    delete

--- main.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Administrator on 2017/3/6 0006.
- */
-
-console.log(require('./support'));
-
-var a = 1;
-var b =2;
--- support.js
+++
@@ -1,6 +0,0 @@
-/**
- *
- * Created by Administrator on 2017/3/6 0006.
- */
-
-module.exports='im support';
